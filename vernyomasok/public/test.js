c6f1f3e374b5bea409ebf14f0c7ec31905e7b6f0
etnevelpriv <leventerastovits@gmail.com>
2026-01-30 08:08:50 +0100

    arrayGeneralas function kesz

--- vernyomasok/public/test.js
+++ vernyomasok/public/test.js
@@ -10,7 +10,13 @@ const randomVernyomasGeneralas = function () {
 
 const arrayGeneralas = function () {
     const arr = [];
-
+    for ( let i = 0; i < 100; i++) {
+        const obj = {
+            nev : randomNevGeneralas(),
+            vernyomas : randomVernyomasGeneralas()
+        };
+        arr.push(obj);
+    };
 };
 
 const arrayMenteseJSONFormatumban = function () {
